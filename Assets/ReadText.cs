5b2446200519ad90b5b12b021e4f6abead279e5a
Maxime Meyrat <maxime.meyrat@etu.u-bordeaux.fr>
2023-02-15 20:10:27 +0100

    clean some code in readtext

--- Assets/ReadText.cs
+++ Assets/ReadText.cs
@@ -11,35 +11,42 @@ public class ReadText : MonoBehaviour
     public TextAsset textFile;
     public BoxCollider collider;
     public GameObject background;
-    private TextMesh textMesh;
 
-    // Start is called before the first frame update
+    private TextMesh textMesh;
+    private int maxLineLength = 20; 
+
+    /**
+    * Start is called before the first frame update
+    **/
     void Start()
     {
         textMesh = GameObject.Find("3DText").GetComponent<TextMesh>();
         string textContent = textFile.text;
-        int width = 20; 
 
-        // format text with a length for each line of 15 characters
-        string formatedText = formatText(textContent, width);
-        int height = getTextHeight(formatedText);
+        // Format the text with a maximum length for each line of 15 characters
+        string formatedText = formatText(textContent);
+        // Get the total number of lines of the text
+        int height = formatedText.Split('\n').Length - 1;
 
         // set text and resize background and collider
         textMesh.text = formatedText;
-        resizeWindow(width, height);
+        resizeWindow(height);
     }
 
     /**
     * Format the text, each line got the same length
+    *
     * @param text : text to format
     * @param lineLength : length of each line of the text
     * @return text in the wanted format
     **/
-    string formatText(string text, int lineLength) {
+    string formatText(string text) {
         var builder = new StringBuilder();
-        for (int i=0; i<text.Length; i++) {
+
+        for (int i = 0; i < text.Length; i++) {
             builder.Append(text[i]);
-            if (i!= 0 && i % lineLength == 0) {
+            
+            if (i != 0 && i % maxLineLength == 0) {
                 builder.Append(Environment.NewLine);
             }
         }
@@ -48,31 +55,20 @@ public class ReadText : MonoBehaviour
     }
 
     /**
-    * Get the number of lines of the text
-    * @param text : text to display in string format 
-    * @return the number of line of this text
-    **/
-    int getTextHeight(string text) {
-        int height = 0;
-        for (int i=0; i<text.Length; i++) {
-            if (text[i] == '\n') {
-                height++;
-            }
-        }
-
-        return height;
-    }
-
-    /**
     * Method to resize the background and the collider containing the text to display
-    * @param width : width of the window
+    * 
     * @param height : height of the window 
     **/
-    void resizeWindow(int width, int height) {
-        print(height);
-        collider.center = new Vector3(width/3.0f, -height+11.0f, 0);
-        background.transform.localPosition = new Vector3(width/3.0f, -height+11.0f, 0.5f);
-        background.transform.localScale = new Vector3(width - 2.0f, 2*height-20.0f, 0.001f);
-        collider.size = new Vector3(width - 2.0f, 2*height-20.0f, 1);
+    void resizeWindow(int height) {
+        float sizeWidthOffset = 2.0f;
+        float sizeHeightOffset = - 20.0f;
+        float centerWidthOffset = 3.0f;
+        float centerHeightOffset = 11.0f;
+
+        collider.center = new Vector3(maxLineLength / centerWidthOffset, - height + centerHeightOffset, 0);
+        collider.size = new Vector3(maxLineLength - sizeWidthOffset, 2 * height + sizeHeightOffset, 1.0f);
+
+        background.transform.localPosition = new Vector3(maxLineLength / centerWidthOffset, - height + centerHeightOffset, 0.5f);
+        background.transform.localScale = new Vector3(maxLineLength - sizeWidthOffset, 2 * height + sizeHeightOffset, 0.001f);
     }
 }
