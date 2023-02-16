943d739e88716cd29c850baec380f2781cc9a254
Maxime Meyrat <maxime.meyrat@etu.u-bordeaux.fr>
2023-02-16 01:02:11 +0100

    add unit tests for readtext

---
+++ Assets/Scripts/DisplayImage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Drawing;
+
+public class DisplayImage : MonoBehaviour
+{
+    private GameObject imagePoster;
+    public Texture texture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imagePoster = GameObject.Find("ImagePoster");
+        imagePoster.GetComponent<Renderer>().material.mainTexture = texture;
+    }
+}
---
+++ Assets/Tests/ReadTextTests.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using System.IO;
+
+
+namespace Tests
+{
+    public class ReadTextTests
+    {
+        [Test]
+        public void ReadCorrectlyTextFromFileTest()
+        {
+            string filePath = "Assets/Texts/test.txt";
+            string textContent = File.ReadAllText(filePath);
+
+            string text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. \r\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+            Assert.AreEqual(textContent, text);
+        }
+
+        [Test]
+        public void ReadIncorrectlyTextFromFileTest()
+        {
+            string filePath = "Assets/Texts/test.txt";
+            string textContent = File.ReadAllText(filePath);
+
+            string text = "This text is incorrect.";
+
+            Assert.AreNotEqual(textContent, text);
+        }
+
+        [Test]
+        public void formatTextCorrectlyTest()
+        {
+            ReadText rt = new ReadText();
+
+            string textToFormat = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            string correctText = "Lorem ipsum dolor sit\r\n amet, consectetur a\r\ndipiscing elit, sed \r\ndo eiusmod tempor in\r\ncididunt ut labore e\r\nt dolore magna aliqu\r\na.";
+
+            Assert.AreEqual(rt.formatText(textToFormat), correctText);
+        }
+
+        [Test]
+        public void formatTextIncorrectlyTest()
+        {
+            ReadText rt = new ReadText();
+
+            string textToFormat = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+            string incorrectText = "Lor\r\nem ipsum dolor sit\r\n amet, co\r\nnsectetur a\r\ndipiscing elit, sed \r\ndo eius\r\nmod tempor in\r\ncididunt ut labore e\r\nt dolore mag\r\nna aliqu\r\na.";
+
+            Assert.AreNotEqual(rt.formatText(textToFormat), incorrectText);
+        }
+    }
+}
