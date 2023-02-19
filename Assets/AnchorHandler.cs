bbfe3f3bb0dff5a919360a29c82af26771d3dc6f
Maxime Dumonteil <maxime.dumonteil@etu.u-bordeaux.fr>
2023-02-19 22:45:52 +0100

    replace anchor button and script for 3DText

---
+++ Assets/AnchorHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+public class AnchorHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject go;
+
+    public void switchMode()
+    {
+        var comp = go.GetComponent<SolverHandler>();
+
+        /*if (comp.enabled)
+        {
+            comp.enabled = false;
+        }
+        else
+        {
+            comp.enabled = true;
+        }*/
+
+        comp.enabled = !comp.enabled;
+    }
+}
