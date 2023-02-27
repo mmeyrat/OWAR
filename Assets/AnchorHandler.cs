5916cbcd7f4b944bda17456e2de6228a45dde9b4
Maxime Dumonteil <maxime.dumonteil@etu.u-bordeaux.fr>
2023-02-28 00:17:56 +0100

    add toggle switch as anchor button on file show prefab

--- Assets/AnchorHandler.cs
+++ Assets/AnchorHandler.cs
@@ -33,10 +33,40 @@ public class AnchorHandler : MonoBehaviour
         
         // Variables
         var comp = anchoredObject.GetComponent<SolverHandler>(); // component that handle view anchor mode, so handle follow movement transformation
-        Color triggerColor = switchModeTrigger.GetComponent<Image>().color; // anchor trigger's color
+        //Color triggerColor = switchModeTrigger.GetComponent<Image>().color; // anchor trigger's color
 
         // Register old button color to reset it later
         // oldButtonColor = buttonColor;
+
+        if (comp.enabled) // if object is view anchored
+        {
+            // Switch to world anchor mode
+            comp.enabled = false;
+
+            //Change "anchor mode" button color to feedback
+            //switchModeTrigger.GetComponent<Image>().color = oldColor;
+
+        }
+        else // if object is world anchored
+        {
+            // Switch to view anchor mode
+            comp.enabled = true;
+
+            //Change "anchor mode" button color to feedback
+            //oldColor = triggerColor;
+            //switchModeTrigger.GetComponent<Image>().color = Color.green;
+        }
+    }
+
+    // To switch anchor mode of anchoredObject and change anchor button color as feedback
+    public void switchMode2()
+    {
+        // TODO FIX this forces to name anchor button "AnchorButton" and not sure it's correct
+        // TODO FIX this forces to use a button to switch anchor mode
+
+        // Variables
+        var comp = anchoredObject.GetComponent<SolverHandler>(); // component that handle view anchor mode, so handle follow movement transformation
+        Color triggerColor = switchModeTrigger.GetComponent<Image>().color; // anchor trigger's color
 
         if (comp.enabled) // if object is view anchored
         {
