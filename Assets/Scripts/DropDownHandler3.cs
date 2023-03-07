4e427828d01fe8505b035ce229f61c1c98b385fe
Maxime Dumonteil <maxime.dumonteil@etu.u-bordeaux.fr>
2023-03-07 10:28:15 +0100

    rework file list in visualizer menu

---
+++ Assets/Scripts/DropDownHandler3.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.EventSystems;
+using UnityEngine.UI.CoroutineTween;
+using System;
+
+public class DropDownHandler3 : MonoBehaviour
+{
+    // To deploy on HoloLens use this path (works also on Unity):
+    private static string path = Application.streamingAssetsPath;
+    private static Dictionary<string, bool> choosenFiles = new Dictionary<string, bool>();
+    private static string[] fileEntries;
+    private static bool alreadyDisplayed = false;
+
+    /**
+    * Start is called before the first frame update
+    **/
+    void Start()
+    {
+        //Dropdown dropdown = transform.GetComponent<Dropdown>();
+        //dropdown.options.Clear();
+
+        // Display all files names available in the dropdown list
+        fileEntries = Directory.GetFiles(path);
+
+        foreach (string file in fileEntries)
+        {
+            if (file.EndsWith(".png") || file.EndsWith(".jpg") || file.EndsWith(".jpeg") || file.EndsWith(".txt"))
+            {
+                //dropdown.options.Add(new Dropdown.OptionData() { text = file.Substring(path.Length + 1) });
+                AddItemToList(file.Substring(path.Length + 1));
+
+                if (alreadyDisplayed == false)
+                {
+                    choosenFiles.Add(file.Substring(path.Length + 1), false);
+                }
+            }
+        }
+
+    }
+
+    public GameObject ListItem;
+
+    public GameObject GlobalListParent;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="file"></param>
+    private void AddItemToList(string file)
+    {
+        // Duplicate item template and add it to the list (and set parent)
+        GameObject NewListItem = Instantiate(ListItem, this.transform.GetChild(0));
+
+        // Set text of instantiated item
+        Text NewListItemText = NewListItem.GetComponentInChildren<Text>();
+        NewListItemText.text = file; // TODO maybe clean by not use a variable
+
+        // Set active the new item 
+        NewListItem.SetActive(true);
+
+        // Set event to toggle on the item
+        var ItemToggle = ListItem.transform.GetChild(2);
+        Toggle ItemToggleComponent = (Toggle)ItemToggle.GetComponent<Toggle>();     
+        
+        /*
+        ItemToggleComponent.onValueChanged.AddListener(delegate {            
+            ToggleValueChanged(ItemToggleComponent);
+        });
+        */
+      
+
+    }
+
+    /**
+   * Return if a file has been already choosen 
+   * 
+   * @param file : the selected file     
+   * @return true or false
+   **/
+    public static bool IsFileChoosen(string file)
+    {
+        return choosenFiles[file];
+    }
+
+    /**
+    * Unselect every file to display the choosen ones
+    **/
+    public static void HasBeenDisplayed()
+    {
+        alreadyDisplayed = true;
+
+        foreach (string file in fileEntries)
+        {
+            choosenFiles[file] = false;
+        }
+    }
+
+    /**
+    * Select a file to display
+    * 
+    * @param file : the file to select
+    **/
+    public static void SetToChoosen(string file)
+    {
+        choosenFiles[file] = !choosenFiles[file];
+    }
+
+    /**
+    * Return the path of the directory where the files are located
+    * 
+    * @return directory path
+    **/
+    public static string GetPath()
+    {
+        return path;
+    }
+
+    /**
+    * Return the files available to display
+    * 
+    * @return an array of files
+    **/
+    public static string[] GetFiles()
+    {
+        fileEntries = Directory.GetFiles(path);
+
+        for (int i = 0; i < fileEntries.Length; i++)
+        {
+            fileEntries[i] = fileEntries[i].Substring(path.Length + 1);
+        }
+
+        return fileEntries;
+    }
+
+}
---
+++ Assets/Scripts/DropDownToVisual2.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System;
+using TMPro;
+
+public class DropDownToVisual2 : MonoBehaviour
+{
+    public Dropdown dropdownList;
+    public Button button;
+    public Text selectedFiles;
+
+    private int maxLineLength = 17;
+
+    /**
+    * Add the selecetd files in a text preview
+    **/
+    public void FileSelector(Text label)
+    {
+        //int index = dropdownList.value;
+        //string file = dropdownList.options[index].text;
+
+        string file = label.text;
+
+        DropDownHandler3.SetToChoosen(file);
+
+        if (DropDownHandler3.IsFileChoosen(file))
+        {
+            selectedFiles.text += $"\n - {file}";
+        }
+        else
+        {
+            selectedFiles.text = selectedFiles.text.Replace($"\n - {file}", "");
+        }
+    }
+
+    /**
+    * Change the scene to display the files
+    **/
+    public void Visualize()
+    {
+        if (selectedFiles.text.Length > maxLineLength)
+        {
+            float offsetImage = 0.2f;
+            float offsetImageIncrement = 3.0f;
+            float offsetText = -0.2f;
+            float offsetTextIncrement = 2.1f;
+            string[] files = DropDownHandler3.GetFiles();
+
+            foreach (string f in files)
+            {
+                if (f.EndsWith(".jpg") || f.EndsWith(".jpeg") || f.EndsWith(".png"))
+                {
+                    if (DropDownHandler3.IsFileChoosen(f))
+                    {
+                        GameObject imagePrefab = Instantiate(Resources.Load("ImagePrefab")) as GameObject;
+                        imagePrefab.transform.localPosition = new Vector3(offsetText, imagePrefab.transform.localPosition.y, imagePrefab.transform.localPosition.z);
+
+                        // Link to close button
+                        imagePrefab.GetComponent<Close>().SetObj(imagePrefab);
+
+                        imagePrefab.GetComponent<DisplayImage>().SetImageObject(imagePrefab.transform.GetChild(0).GetChild(0).GetComponent<RawImage>());
+                        imagePrefab.GetComponent<DisplayImage>().SetFileName(Path.Combine(DropDownHandler3.GetPath(), f));
+                        imagePrefab.GetComponent<DisplayImage>().SetPoseX(offsetImage);
+
+                        offsetImage *= offsetImageIncrement;
+
+                        selectedFiles.text = selectedFiles.text.Replace($"\n - {f}", "");
+                        DropDownHandler3.SetToChoosen(f);
+                    }
+                }
+                else if (f.EndsWith(".txt"))
+                {
+                    if (DropDownHandler3.IsFileChoosen(f))
+                    {
+                        GameObject textPrefab = Instantiate(Resources.Load("TextPrefab")) as GameObject;
+                        textPrefab.transform.localPosition = new Vector3(offsetText, textPrefab.transform.localPosition.y, textPrefab.transform.localPosition.z);
+
+                        // Link to close button
+                        textPrefab.GetComponent<Close>().SetObj(textPrefab);
+                        // Link to next page button
+                        textPrefab.GetComponent<ChangePage>().SetObj(textPrefab.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>());
+
+                        textPrefab.GetComponent<ReadText>().SetTextObject(textPrefab.transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>());
+                        textPrefab.GetComponent<ReadText>().SetFileName(Path.Combine(DropDownHandler3.GetPath(), f));
+
+                        offsetText *= offsetTextIncrement;
+
+                        selectedFiles.text = selectedFiles.text.Replace($"\n - {f}", "");
+                        DropDownHandler3.SetToChoosen(f);
+                    }
+                }
+            }
+        }
+        else
+        {
+            // TODO : Popup to show alert no files selected
+        }
+    }
+}
