d80ce8ac7c2643e9e1a6367323cd0afeaca339a6
Maxime Meyrat <maxime.meyrat@etu.u-bordeaux.fr>
2023-03-20 17:11:49 +0100

    remove prefab from tag area on move

---
+++ Assets/Scripts/ManipulatorHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManipulatorHandler : MonoBehaviour
+{   
+    private int minTagAreaId = 0;
+
+    public void RemoveFromTagAreaSlot()
+    {
+        int tagAreaId = this.GetComponent<PrefabData>().GetTagAreaId();
+        int slotId = this.GetComponent<PrefabData>().GetTagAreaSlotId();
+
+        if (tagAreaId >= minTagAreaId)
+        {
+            if (!TagSceneHandler.GetTagAreaList()[tagAreaId].GetSlotAvailability(slotId))
+            {
+                TagSceneHandler.GetTagAreaList()[tagAreaId].SetSlotAvailability(slotId, true);
+            }
+        }
+    }
+}
